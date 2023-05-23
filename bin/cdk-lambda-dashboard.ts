96848e0950dfe037a3e68a37ce136a767a4394f9
sebastientromp <sebastien.tromp@gmail.com>
2023-05-23 16:15:12 +0200

    customize dashboard for project's lambdas

--- bin/cdk-lambda-dashboard.ts
+++ bin/cdk-lambda-dashboard.ts
@@ -1,16 +1,223 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { CdkLambdaDashboardStack } from '../lib/cdk-lambda-dashboard-stack';
-import { TransactionsAPI } from '../lib/cdk-transactions-api-lambda-construct';
+import * as cdk from "@aws-cdk/core";
+import { CdkLambdaDashboardStack } from "../lib/cdk-lambda-dashboard-stack";
 
 const app = new cdk.App();
 
-const lambdaDashboardStack = new CdkLambdaDashboardStack(app, 'TransactionsLambdaDashboardStack', {
-    dashboardName: "TransactionsLambdaDashboard"
-});
+const lambdaDashboardStack = new CdkLambdaDashboardStack(
+  app,
+  "LambdaDashboardStack",
+  {
+    dashboardName: "LambdaDashboard",
+  }
+);
 
-const transactionsApi = new TransactionsAPI(lambdaDashboardStack, "TransactionsApi");
+// const transactionsApi = new TransactionsAPI(
+//   lambdaDashboardStack,
+//   "TransactionsApi"
+// );
+// lambdaDashboardStack.addLambda("BeginTransaction", "BeginTransaction");
+// lambdaDashboardStack.addLambda("UpdateTransaction", "UpdateTransaction");
+// lambdaDashboardStack.addLambda("GetTransaction", "GetTransaction");
 
-lambdaDashboardStack.addLambda("BeginTransaction", "BeginTransaction");
-lambdaDashboardStack.addLambda("UpdateTransaction", "UpdateTransaction");
-lambdaDashboardStack.addLambda("GetTransaction", "GetTransaction");
+lambdaDashboardStack.addLambda(
+  "CreateFullReviewStack-PopulateGameSummaryFunction-1CKMNQ86EWASV",
+  "trigger-create-full-review"
+);
+
+lambdaDashboardStack.addLambda(
+  "AuthStack-AuthFunction-Q68CiWS0cRqs",
+  "auth-website"
+);
+lambdaDashboardStack.addLambda(
+  "BuildBattlegroundsHeroSta-BuildBgsHeroStatsFunctio-1SZDUSKPTX915",
+  "cron-build-bgs-hero-stats"
+);
+lambdaDashboardStack.addLambda(
+  "CronExportStatsStack-ExportHsDecks-SQQQwX0TL7eF",
+  "cron-export-stats-hsdecks"
+);
+lambdaDashboardStack.addLambda(
+  "DuelsLeaderboardStack-RetrieveDuelsLeaderboardFunc-gVud1Wo2IE8j",
+  "api-retrieve-duels-leaderboard-2"
+);
+lambdaDashboardStack.addLambda(
+  "FeedbackStack-ProcessFeedbackFunction-13539NTZ0TH39",
+  "api-feedback"
+);
+lambdaDashboardStack.addLambda(
+  "IdFromBgsSimulationSample-GetIdFromBgsSimulationSa-10AGMMKZ66GWW",
+  "api-save-bgs-simulation-sample"
+);
+lambdaDashboardStack.addLambda(
+  "IdFromBgsSimulationSample-GetIdFromBgsSimulationSa-XdHoinVSW1lU",
+  "api-bgs-get-id-from-sample-2"
+);
+lambdaDashboardStack.addLambda(
+  "MRLambdaStack-AggregatorFunction-1MAQYAZ77F682",
+  "mr-lambda-aggregator"
+);
+lambdaDashboardStack.addLambda(
+  "MRLambdaStack-DriverFunction-1HMIH97JIJS2A",
+  "mr-lambda-driver"
+);
+lambdaDashboardStack.addLambda(
+  "MRLambdaStack-MapperFunction-1NU11S9BLCB9B",
+  "mr-lambda-mapper"
+);
+lambdaDashboardStack.addLambda(
+  "MRLambdaStack-MapperWatcherFunction-1RQPAF6SW7ZEC",
+  "mr-lambda-mapper-watcher"
+);
+lambdaDashboardStack.addLambda(
+  "MRLambdaStack-ReducerFunction-1RUWC6SIUC6DN",
+  "mr-lambda-reducer"
+);
+lambdaDashboardStack.addLambda(
+  "MatchStatsStack-ProcessMatchStatsFunction2-8qoxF7NzjWs5",
+  "api-retrieve-user-match-stats-2"
+);
+lambdaDashboardStack.addLambda(
+  "PublishBgsPerfectGameStac-BuildBgsPerfectGamesFunc-FGdcis7zkK8W",
+  "cron-build-bgs-perfect-games"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveBgsSimulationSamp-RetrieveBgsSimulationSam-NB5kBWGr0f0I",
+  "api-bgs-retrieve-simulation-sample-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveDuelsSingleRunSta-RetrieveDuelsSingleRunFu-ixek2vFX6Dlu",
+  "api-retrieve-duels-single-run-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveGlobalStatsStack-ProcessGlobalStatsFunctio-13LLAFGKONTIZ",
+  "api-retrieve-user-global-stats"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveGlobalStatsStack-RetrieveGlobalStatsFuncti-XoTxr4I7PIwH",
+  "api-retrieve-overview-global-stats-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveReviewStack-RetrieveReviewFunction2-JWlhajdkQTjc",
+  "api-retrieve-review-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveUserBgsBestStatsS-RetrieveUserBgsBestStats-F21W8EPJLG9P",
+  "api-retrieve-user-bgs-best-stats"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveUserBgsBestStatsS-RetrieveUserBgsBestStats-tkbV418sS3Lg",
+  "api-retrieve-user-bgs-best-stats-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveUsersDuelsRunsSta-RetrieveUserDuelsRunFunc-7CGn1JSO1Mmx",
+  "api-retrieve-user-duels-runs-2"
+);
+lambdaDashboardStack.addLambda(
+  "RetrieveUsersDuelsRunsSta-RetrieveUserDuelsRunFunc-QEE0W6SAZ3M",
+  "api-retrieve-user-match-stats"
+);
+lambdaDashboardStack.addLambda(
+  "SimulateBgsBattleStack-SimulateBgsBattleFunctionLi-BhXWqLooZGey",
+  "api-simulate-battlegrounds-battle-2"
+);
+lambdaDashboardStack.addLambda(
+  "SyncDataStack-ProcessSyncStatsFunction-sEKcsqtW3rys",
+  "process-sync-stats"
+);
+lambdaDashboardStack.addLambda(
+  "TriggerBuildDuelsStatsStac-BuildDuelsStatsFunction-C99P6IG8LRFL",
+  "cron-build-duels-stats"
+);
+lambdaDashboardStack.addLambda(
+  "TriggerBuildMercenariesSt-BuildMercenariesStatsFun-lJdHCPY81s31",
+  "cron-build-mercenaries-stats"
+);
+lambdaDashboardStack.addLambda(
+  "TwitchPresenceStack-ProcessSaveTwitchPresenceFunct-CTG1cCc2BcWb",
+  "process-save-twitch-presence"
+);
+lambdaDashboardStack.addLambda(
+  "TwitchPresenceStack-RetrieveTwitchPresenceFunction-asbSWYenLgW0",
+  "api-retrieve-twitch-presence-2"
+);
+lambdaDashboardStack.addLambda(
+  "TwitchPresenceStack-StartSaveTwitchPresenceFunctio-hq7XqPkpeOB0",
+  "api-start-save-twitch-presence"
+);
+lambdaDashboardStack.addLambda(
+  "TwitchPresenceStack-StartSaveTwitchPresenceFunctio-lcpZzlXVCHOj",
+  "api-start-save-twitch-presence-2"
+);
+lambdaDashboardStack.addLambda(
+  "TwitterStack-BuildTwitterFunction-00rWH4qXQgww",
+  "cron-build-twitter"
+);
+lambdaDashboardStack.addLambda(
+  "UserAchievementsStack-RetrieveAchievementsFunction-4WKJNDR8R6D2",
+  "api-retrieve-achievements"
+);
+lambdaDashboardStack.addLambda(
+  "UserAchievementsStack-RetrieveAchievementsFunction-tlzvG2EWGUci",
+  "api-retrieve-achievements-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserAchievementsStack-StartSaveAchievementsFunctio-1Q0LH8YML57AP",
+  "api-start-save-achievements"
+);
+lambdaDashboardStack.addLambda(
+  "UserArenaRewardsStack-RetrieveArenaRewardsFunction-yu1culDYtDZb",
+  "api-retrieve-arena-rewards-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserArenaRewardsStack-StartSaveArenaRewardsFunctio-QaYyx36W2LKg",
+  "api-start-save-arena-rewards"
+);
+lambdaDashboardStack.addLambda(
+  "UserArenaRewardsStack-StartSaveArenaRewardsFunctio-iSDQqoK8wzIs",
+  "api-start-save-arena-rewards-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserBgsPostMatchStatsStac-ProcessSaveBgsPostMatchS-1PD9UEYMIOSQ8",
+  "process-save-bgs-post-match-stats"
+);
+lambdaDashboardStack.addLambda(
+  "UserBgsPostMatchStatsStac-RetrieveBgsPostMatchStat-A84Wj7WJ7NqZ",
+  "api-retrieve-bgs-post-match-stats-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserBgsPostMatchStatsStac-RetrieveBgsPostMatchStat-MC9A0ZZB1CBQ",
+  "api-retrieve-bgs-post-match-stats"
+);
+lambdaDashboardStack.addLambda(
+  "UserBgsPostMatchStatsStac-StartSaveBgsPostMatchSta-U1NFYLWK3E0N",
+  "api-start-save-bgs-post-match-stats"
+);
+lambdaDashboardStack.addLambda(
+  "UserBgsPostMatchStatsStac-StartSaveBgsPostMatchSta-s4bDntWeqkXH",
+  "api-start-save-bgs-post-match-stats-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserMappingStack2-StartSaveMappingFunction-1XPZIJ8YG29RH",
+  "api-start-save-mapping"
+);
+lambdaDashboardStack.addLambda(
+  "UserMappingStack2-StartSaveMappingFunction2-N8wDp6ws7NqN",
+  "api-start-save-mapping-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserPacksStack-ProcessSavePacksFunction-1CBREIG8IM3RF",
+  "process-save-packs"
+);
+lambdaDashboardStack.addLambda(
+  "UserPrefsStack-RetrievePrefsFunction2-vsyqbBbNwpa5",
+  "api-retrieve-prefs-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserPrefsStack-StartSavePrefsFunction2-BAihDOyl63Qj",
+  "api-start-save-prefs-2"
+);
+lambdaDashboardStack.addLambda(
+  "UserProfileStack-RetrieveOwnProfileFunction-OPzqESAYjDju",
+  "api-retrieve-own-profile"
+);
--- lib/cdk-lambda-dashboard-stack.ts
+++ lib/cdk-lambda-dashboard-stack.ts
@@ -1,134 +1,129 @@
-import * as cdk from '@aws-cdk/core';
-import { Dashboard, GraphWidget, Metric } from '@aws-cdk/aws-cloudwatch';
+import {
+  Dashboard,
+  GraphWidget,
+  MathExpression,
+  Metric,
+} from "@aws-cdk/aws-cloudwatch";
+import * as cdk from "@aws-cdk/core";
 
 export interface LambdaDashboardsStackProps extends cdk.StackProps {
-    dashboardName: string;
+  dashboardName: string;
 }
 
 export class CdkLambdaDashboardStack extends cdk.Stack {
+  protected readonly lambdaDashboard: Dashboard;
 
-    protected readonly lambdaDashboard: Dashboard;
+  protected readonly invocations = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "Invocations",
+    statistic: "sum",
+  });
 
-    protected readonly invocations = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "Invocations",
-        statistic: "sum"
+  // protected readonly duration = new Metric({
+  //     namespace: "AWS/Lambda",
+  //     metricName: "Duration",
+  //     statistic: "min"
+  // });
+
+  protected readonly errors = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "Errors",
+    statistic: "sum",
+    color: "#d13212",
+  });
+
+  //   protected readonly availability = new Metric({
+  //     namespace: "AWS/Lambda",
+  //     metricName: "Success rate (%)",
+  //     label: "Success rate (%)",
+  //     statistic: "sum",
+  //     color: "#2ca02c",
+  //   });
+
+  protected readonly availability = (functionName: string) =>
+    new MathExpression({
+      label: "Success rate (%)",
+      color: "#2ca02c",
+      expression: "100 - 100 * errors / MAX([errors, invocations])",
+      usingMetrics: {
+        errors: this.errors.with({
+          dimensions: { FunctionName: functionName },
+        }),
+        invocations: this.invocations.with({
+          dimensions: { FunctionName: functionName },
+        }),
+      },
     });
 
-    protected readonly duration = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "Duration",
-        statistic: "min"
+  protected readonly throttles = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "Throttles",
+    statistic: "sum",
+  });
+
+  protected readonly provisionedConcurrencySpillovers = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "ProvisionedConcurrencySpilloverInvocations",
+    statistic: "sum",
+  });
+
+  protected readonly concurrentExecutions = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "ConcurrentExecutions",
+    statistic: "sum",
+  });
+
+  protected readonly provisionedConcurrentExecutions = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "ProvisionedConcurrentExecutions",
+    statistic: "sum",
+  });
+
+  protected readonly provisionedConcurrencyUtilization = new Metric({
+    namespace: "AWS/Lambda",
+    metricName: "ProvisionedConcurrencyUtilization",
+    statistic: "sum",
+  });
+
+  constructor(scope: cdk.App, id: string, props: LambdaDashboardsStackProps) {
+    super(scope, id, props);
+
+    this.lambdaDashboard = new Dashboard(this, props.dashboardName, {
+      dashboardName: props.dashboardName,
     });
+  }
 
-    protected readonly errors = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "Errors",
-        statistic: "sum"
-    });
+  // adds one row to dashboard for each lambda function
+  public addLambda(functionName: string, displayName: string) {
+    const dimensions = {
+      FunctionName: functionName,
+    };
 
-    protected readonly throttles = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "Throttles",
-        statistic: "sum"
-    });
+    this.lambdaDashboard.addWidgets(
+      new GraphWidget({
+        title: displayName + " Invocations",
+        left: [
+          this.invocations.with({
+            dimensions: dimensions,
+          }),
+        ],
+      }),
 
-    protected readonly provisionedConcurrencySpillovers = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "ProvisionedConcurrencySpilloverInvocations",
-        statistic: "sum"
-    });
-
-    protected readonly concurrentExecutions = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "ConcurrentExecutions",
-        statistic: "sum"
-    });
-
-    protected readonly provisionedConcurrentExecutions = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "ProvisionedConcurrentExecutions",
-        statistic: "sum"
-    });
-
-    protected readonly provisionedConcurrencyUtilization = new Metric({
-        namespace: "AWS/Lambda",
-        metricName: "ProvisionedConcurrencyUtilization",
-        statistic: "sum"
-    });
-
-    constructor(scope: cdk.App, id: string, props: LambdaDashboardsStackProps) {
-        super(scope, id, props);
-
-        this.lambdaDashboard = new Dashboard(this, props.dashboardName, {
-            dashboardName: props.dashboardName
-        });
-    }
-
-    // adds one row to dashboard for each lambda function
-    public addLambda(functionName: string, displayName: string) {
-
-        const dimensions = {
-            "FunctionName": functionName
-        };
-
-        this.lambdaDashboard.addWidgets(
-            new GraphWidget({
-                title: displayName + " Invocations",
-                left: [
-                    this.invocations.with({
-                        dimensions: dimensions,
-                    }),
-
-                ]
-            }),
-
-            new GraphWidget({
-                title: displayName + " Duration",
-                left: [
-                    this.duration.with({
-                        dimensions: dimensions,
-                    }),
-                    this.duration.with({
-                        dimensions: dimensions,
-                        statistic: "avg"
-                    }),
-                    this.duration.with({
-                        dimensions: dimensions,
-                        statistic: "max"
-                    }),
-                ]
-            }),
-
-            new GraphWidget({
-                title: displayName + " Errors",
-                left: [
-                    this.errors.with({
-                        dimensions: dimensions,
-                    }),
-                    this.throttles.with({
-                        dimensions: dimensions,
-                    }),
-                    this.provisionedConcurrencySpillovers.with({
-                        dimensions: dimensions,
-                    })
-                ]
-            }),
-
-            new GraphWidget({
-                title: displayName + " ConcurrentExecutions",
-                right: [
-                    this.concurrentExecutions.with({
-                        dimensions: dimensions,
-                    }),
-                    this.provisionedConcurrentExecutions.with({
-                        dimensions: dimensions,
-                    }),
-                    this.provisionedConcurrencyUtilization.with({
-                        dimensions: dimensions,
-                    })
-                ]
-            }),
-        );
-    }
+      new GraphWidget({
+        title: displayName + " Errors",
+        left: [
+          this.errors.with({
+            dimensions: dimensions,
+          }),
+          //   this.throttles.with({
+          //     dimensions: dimensions,
+          //   }),
+          //   this.provisionedConcurrencySpillovers.with({
+          //     dimensions: dimensions,
+          //   }),
+        ],
+        right: [this.availability(functionName)],
+      })
+    );
+  }
 }
